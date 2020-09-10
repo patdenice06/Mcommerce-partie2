f7c2c1856a726af76f1b0359b5ab608cd22f2b41
Patrick GIRAUD <patrickgiraud@hotmail.com>
2020-09-10 18:46:23 +0200

    Create Feign client UI for microservices

---
+++ clientui/src/test/java/com/clientui/ClientUiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.clientui;
+
+// import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ClientUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
