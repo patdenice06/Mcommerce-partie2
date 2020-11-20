20215bb5a116e79da8b4964ceeaeaa33a70bb340
Patrick GIRAUD <patrickgiraud@hotmail.com>
2020-11-20 19:17:36 +0100

    Initiate Ribbon Load Balancing for 2 instances of microservices mproduits

--- clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
@@ -2,13 +2,15 @@ package com.clientui.proxies;
 
 import java.util.List;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.clientui.beans.ProductBean;
 
-@FeignClient(name = "microservice-produits", url = "localhost:9001")
+@FeignClient(name = "microservice-produits")
+@RibbonClient(name = "microservice-produits")
 public interface MicroserviceProduitsProxy {
 
 	@GetMapping(value = "/Produits")
