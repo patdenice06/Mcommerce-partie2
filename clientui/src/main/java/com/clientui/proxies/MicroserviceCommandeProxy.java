97c1f17c12486ef4dad8ba1b95c28a7dec6018fb
Patrick GIRAUD <patrickgiraud@hotmail.com>
2020-11-23 18:17:26 +0100

    Update for Ribbon client

--- clientui/src/main/java/com/clientui/proxies/MicroserviceCommandeProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceCommandeProxy.java
@@ -5,13 +5,16 @@ import com.clientui.beans.CommandeBean;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "microservice-commandes", url = "localhost:9002")
+@FeignClient(name = "microservice-commandes")
+@RibbonClient(name = "microservice-commandes")
+
 public interface MicroserviceCommandeProxy {
 
     @PostMapping(value = "/commandes")
--- clientui/src/main/java/com/clientui/proxies/MicroservicePaiementProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroservicePaiementProxy.java
@@ -4,13 +4,15 @@ import com.clientui.beans.PaiementBean;
 
 import java.util.List;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "microservice-paiement", url = "localhost:9003")
+@FeignClient(name = "microservice-paiement")
+@RibbonClient(name = "microservice-paiement")
 public interface MicroservicePaiementProxy {
 
     @PostMapping(value = "/paiement")
